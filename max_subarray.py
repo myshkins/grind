53dff44ded0ee0792535f2a7cfa7af13d547c9d5
mrpoopy <junk@krenitsky.org>
2022-11-01 18:14:49 -0400

    workin the max strat

--- max_subarray.py
+++ max_subarray.py
@@ -1,27 +1,33 @@
 import csv
 
 class Solution:
+    def __init__(self):
+        self.i = 0
+        self.j = 0
+        self.p = 0
+        self.q = 0
+        
     def maxSubArray(self, nums: list) -> int:
         i, j = 0, 0 #pointer for compares
         p = (len(nums) - 1) #pointers for maxSubArray
         q = p
         total = sum(nums)
+        maximum = total
+        # a, b = 0, 0     #subarray indices
         while i < p:
-            if (tailsum := sum(nums[j:i + 1])) <= (headsum := sum(nums[p:q + 1])):
-                if total + (tailsum) < total:
-                    i += 1
-                    j = i
-                    total -= tailsum
-                else:
-                    i += 1
-            else:
-                if total + (headsum) < total:
-                    p -= 1
-                    q = p
-                    total -= headsum
-                else:
-                    p -= 1
-        return total
+            if (tailsum := sum(nums[j:i + 1])) > maximum:
+                maximum = tailsum
+                # a, b = j, i
+            if (headsum := sum(nums[p:q + 1])) > maximum:
+                maximum = headsum
+                # a, b = p, q
+            if tailsum <= 0:
+                j = i
+            if headsum <= 0:
+                q = p
+            i += 1
+            p -= 1
+        return maximum
 
     def read_input(self):
         with open('max_subarray_input.txt', mode='r') as file:
@@ -32,15 +38,23 @@ class Solution:
             lst = list(map(int, lst[0]))
             return lst
 
+    def sum(self, nums):
+        print(sum(nums))
+
 nums4 = [-2, -3, -1]
 #output -1
 nums3 = [1, -1, 1]
 #output 1            
 nums2 = [-1, -2]
-#Output: 6
+#Output: -1
 nums1 = [-2,1,-3,4,-1,2,1,-5,4]
 #Output: 6
 nums6 = [-3, 1, -2, 2]
+#output: 2
+nums7 = [-1, -1, -2, -2]
+nums8 = [0, 0, -3, 1]
+nums9 = [3,-2,-3,-3,1,3,0]
+numsA = [2,-1,-1,2,0,-3,3]
 sol = Solution()
 # nums5 = sol.read_input()
 print(sol.maxSubArray(nums1))
--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,13 +1,13 @@
 class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+        # x = [c.lower() for c in s if c.isalnum()]
+        # front = x[0:(len(x) // 2)]
+        # back = x[-1:-(len(x) // 2 + 1):-1]
+        # return (front == back)
+
     def isPalindrome(self, s: str) -> bool:
         x = [c.lower() for c in s if c.isalnum()]
-        front = x[0:(len(x) // 2)]
-        back = x[-1:-(len(x) // 2 + 1):-1]
-        return (front == back)
-
-    # def isPalindrome(self, s: str) -> bool:
-    #     x = [c.lower() for c in s if c.isalnum()]
-    #     return x[0:(len(x) // 2)] == x[-1:-(len(x) // 2 + 1):-1]
+        return x[0:(len(x) // 2)] == x[-1:-(len(x) // 2 + 1):-1]
 
 
 test1 = "A man, a plan, a canal: Panama"
